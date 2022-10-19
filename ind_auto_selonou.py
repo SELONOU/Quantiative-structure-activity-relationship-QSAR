b9c91e6959d896b22da4da93aec2d687af5c33a4
KANKİNOU SELONOU GAUTİER <kankinounoatedegautier@yahoo.fr>
2022-10-19 09:40:48 +0300

    Add files via upload

--- ind_auto_selonou.py
+++ ind_auto_selonou.py
@@ -5,8 +5,9 @@ df2=df[1]
 print(df1)
 print(df2)
 with open("indices_input_files.txt", "w") as f:
-    for file_name in df1:
-        for number in df2:
+    for i in range(len(df)):
+            file_name=df[0][i]
+            number=df[1][i]
             f.write("@echo off\n")
             f.write("set logfile="+file_name+"sp.log\n")
             f.write("set sq=sq.dat\n")
