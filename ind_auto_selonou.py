ab0daa928f9365d815f5c16d0a35eb4dddcd508c
KANKİNOU SELONOU GAUTİER <kankinounoatedegautier@yahoo.fr>
2022-10-20 19:33:09 +0300

    Add files via upload

--- ind_auto_selonou.py
+++ ind_auto_selonou.py
@@ -1,5 +1,5 @@
 import pandas as pd
-df=pd.read_csv('atoms.csv', sep=",", header=None, usecols=[0,1])
+df=pd.read_csv('input_indices.csv', sep=",", header=None, usecols=[0,1])
 df1=df[0]
 df2=df[1]
 print(df1)
@@ -12,9 +12,9 @@ with open("indices_input_files.txt", "w") as f:
             f.write("set logfile="+file_name+"sp.log\n")
             f.write("set sq=sq.dat\n")
             f.write("set ind="+file_name+"sp.ind\n")
-            f.write("set ind="+file_name+"sp.sal\n")
+            f.write("set sal="+file_name+"sp.sal\n")
             f.write("set natom="+"0"+str(number)+"\n")
             f.write("set input=0\n")
             f.write("set basis=03\n")
-            f.write("(echo %natom% && echo %input% && echo %basis%) | INDICES-AGOSTO-GASTONK.exe %logfile% %sq% %ind% %sal%)\n")
+            f.write("(echo %natom% && echo %input% && echo %basis%) | INDICES-AGOSTO-GASTONK.exe %logfile% %sq% %ind% %sal%\n")
     f.write("\n")
